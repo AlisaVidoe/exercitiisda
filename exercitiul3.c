e70c39cf880cf43e6c6c822d6ae70705420ccd9f
AlisaVidoe <alisavidoe@yahoo.com>
2016-03-13 16:52:58 +0200

    Create exercitiul3.c

---
+++ exercitiul3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char sir[256];
+    int i,lungime;
+    scanf("%s",sir);
+    lungime=strlen(sir);
+    for(i=0;i<lungime;i++)
+    {
+        if(sir[i]<=57 && sir[i]>=48)
+        {
+            printf("%c",sir[i]);
+        }
+    }
+    return 0;
+}
